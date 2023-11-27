d98aa48c46dd91b80754e93a50e0b4e503b31c06
Brad Barnett <127794626+bdbarnett@users.noreply.github.com>
2023-11-26 21:25:46 -0600

    Add files via upload

--- examples/configs/wt32-sc01-plus.py
+++ examples/configs/wt32-sc01-plus.py
@@ -7,6 +7,15 @@ import mpdisplay
 from st7796 import init_sequence, rotations
 from backlight import Backlight
 from machine import Pin
+from time import sleep
+
+# The WT32-SC01 Plus has the reset pins of the display IC and the touch IC both
+# tied to pin 4.  Controlling this pin with the display driver can lead to an
+# unresponsive touchscreen.  This case is not common.  If they aren't tied 
+# together on your board, define reset in mpdisplay.Display instead, like:
+#    mpdisplay.Display(reset=4)
+# Also do this for the display power pin if your board has one.  Most don't.
+reset=Pin(4, Pin.OUT, value=1)
 
 bus = mpdisplay.I80_bus(
     (9, 46, 3, 8, 18, 17, 16, 15),
@@ -23,7 +32,7 @@ display_drv = mpdisplay.Display(
     width=320,
     height=480,
     bpp=16,
-    reset=4,
+    reset=-1,
     rotation=0,
     bgr=True,
     invert_color=True,
