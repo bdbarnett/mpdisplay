9b9496af90b26042a0fc52eda90e08ce85400065
Brad Barnett <bbarnett@gmail.com>
2023-12-23 13:06:56 -0600

    add board_config

---
+++ board_configs/esp32-s3-lcd-4.3/board_config.py
@@ -0,0 +1,73 @@
+""" Waveshare ESP32-S3-Touch-LCD-4.3 800x480 ST7701 display """
+
+from lcd_bus import RGBBus
+from busdisplay import BusDisplay
+from machine import Pin
+from gt911 import GT911  # TODO: port to MicroPython from https://github.com/adafruit/Adafruit_CircuitPython_CST8XX
+
+
+display_bus = RGBBus(
+    hsync=46,
+    vsync=3,
+    de=5,
+    disp=-1,
+    pclk=7,
+    data0=14,
+    data1=38,
+    data2=18,
+    data3=17,
+    data4=10,
+    data5=39,
+    data6=0,
+    data7=45,
+    data8=48,
+    data9=47,
+    data10=21,
+    data11=1,
+    data12=2,
+    data13=42,
+    data14=41,
+    data15=40,
+    freq=21_000_000,
+    num_fbs=2,
+    bb_size_px=0,
+    hsync_front_porch=50,
+    hsync_pulse_width=30,
+    hsync_pulse_width=1,
+    hsync_idle_low=False,
+    vsync_front_porch=20,
+    vsync_back_porch=30,
+    vsync_pulse_width=1,
+    vsync_idle_low=False,
+    de_idle_high=False,
+    pclk_idle_high=False,
+    pclk_active_neg=False,
+    disp_active_low=False,
+    refresh_on_demand=False,
+    fb_in_psram=False,
+    no_fb=False,
+    bb_inval_cache=False,
+)
+
+display_drv = BusDisplay(
+    display_bus,
+    width=800,
+    height=480,
+    colstart=0,
+    rowstart=0,
+    rotation=-1,  # PORTRAIT
+    color_depth=16,
+    color_order=0x08,  # COLOR_ORDER_BGR
+    reverse_bytes_in_word=False,
+    brightness=1.0,
+    backlight_pin=None,
+    backlight_on_high=True,
+    reset_pin=None,
+    reset_high=True,
+    power_pin=None,
+    power_on_high=True,
+)
+
+i2c = I2C(0, scl=Pin(9), sda=Pin(8))
+touch_drv = GT911(i2c)
+touch_drv_read = touch_drv.touches
