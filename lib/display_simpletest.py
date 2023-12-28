a797663b9c90a834cbf968c37bf5bf368e276ffe
Brad Barnett <bbarnett@gmail.com>
2023-12-28 05:23:47 -0600

    support non-ESP32 boards

--- lib/display_simpletest.py
+++ lib/display_simpletest.py
@@ -1,9 +1,17 @@
 """ display_simpletest"""
 
 from board_config import display_drv
-from heap_caps import malloc, CAP_DMA
 import random
 
+try:
+    from heap_caps import malloc, CAP_DMA, CAP_INTERNAL  # For allocating buffers for the blocks and text
+    create_buffer = lambda size: malloc(size, CAP_DMA | CAP_INTERNAL)
+except:
+    create_buffer = lambda size: bytearray(size)
+
+# If the are incorrect, change this value.
+# You may need to change the `reverse_bytes_in_word` value in board_config.py as well
+swap_color_bytes = False
 
 # Get the display dimensions
 display_width = display_drv.width
@@ -26,11 +34,11 @@ block_size = 64   # Size of each dimension in pixels
 block_pixels = block_size * block_size
 blocks = []
 # for color in [random.randint(0, 65535)] * 10:
-for color in [BLACK, WHITE, MAGENTA, CYAN, YELLOW, PURPLE, GREEN, BLUE, RED, ORANGE]:
-    block = malloc(block_pixels*2, CAP_DMA)
+for pixel_color in [BLACK, WHITE, MAGENTA, CYAN, YELLOW, PURPLE, GREEN, BLUE, RED, ORANGE]:
+    block = create_buffer(block_pixels*2)
     for i in range(block_pixels):
-        block[i*2] = color & 0xff
-        block[i*2+1] = color >> 8
+        block[i*2] = pixel_color & 0xff if not swap_color_bytes else pixel_color >> 8
+        block[i*2+1] = pixel_color >> 8 if not swap_color_bytes else pixel_color & 0xff
     blocks.append(block)
 
 # Infinite loop
