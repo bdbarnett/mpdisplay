2e3cade15c21f8c347a53ef58268642924a84899
Brad Barnett <bbarnett@gmail.com>
2023-12-29 02:26:04 -0600

    minor chsnges

--- bus_drivers/lcd_bus.py
+++ bus_drivers/lcd_bus.py
@@ -16,10 +16,12 @@ import struct
 class Optional:
     pass
 
+
 class _BaseBus:
     """
     Base class for bus communication. This class is meant to be subclassed by specific bus implementations.
     """
+
     def __init__(self) -> None:
         """
         Initialize the base bus.
@@ -27,22 +29,24 @@ class _BaseBus:
         self.buf1: bytearray = bytearray(1)
         self.trans_done: bool = True
         self.callback: Optional[callable] = None
+        self.swap_bytes: callable = self._no_swap
 
     def init(
-            self,
-            width: int,  # Not Used
-            height: int,  # Not Used
-            bpp: int,  # Not Used
-            buffer_size: int,
-            rgb565_byte_swap: bool,
-            ) -> None:
+        self,
+        width: int,  # Not Used
+        height: int,  # Not Used
+        bpp: int,  # Not Used
+        buffer_size: int,
+        rgb565_byte_swap: bool,
+    ) -> None:
         """
         Initialize the bus with the given parameters.
         """
-        self.max_transfer_sz: int = buffer_size
         self._rgb565_byte_swap: bool = rgb565_byte_swap
+
         if self._rgb565_byte_swap:
             print("WARNING: rgb565_byte_swap is enabled. This is VERY slow!")
+            self.swap_bytes = self._swap_bytes_in_place
 
     def register_callback(self, callback: callable) -> None:
         """
@@ -59,16 +63,20 @@ class _BaseBus:
         self.trans_done = True
         return False
 
-    @staticmethod
-    def rgb565_byte_swap(buf: memoryview, buf_size_px: int) -> None:
+    def _swap_bytes_in_place(self, buf: memoryview, buf_size_px: int) -> memoryview:
         """
         Swap the bytes in a buffer of RGB565 data.
         VERY slow!!!!!!!!!
         """
-        while buf_size_px:
-            buf[0], buf[1] = buf[1], buf[0]
-            buf = buf[2:]
-            buf_size_px -= 1
+        for i in range(0, buf_size_px * 2, 2):
+            buf[i], buf[i+1] = buf[i+1], buf[i]
+        return buf
+
+    def _no_swap(self, buf: memoryview, buf_size_px: int) -> memoryview:
+        """
+        Do nothing to the buffer.
+        """
+        return buf
 
     def tx_color(self, *_, **__) -> None:
         """
@@ -87,7 +95,7 @@ class _BaseBus:
         Receive parameters. Not yet implemented.
         """
         raise NotImplementedError("Haven't implemented rx_param yet")
-    
+
 
 class SPIBus(_BaseBus):
     """
@@ -113,28 +121,29 @@ class SPIBus(_BaseBus):
         quad_spi (bool, optional): Whether to use quad SPI mode. Defaults to False.
         sio_mode (bool, optional): Whether to use SIO mode. Defaults to False.
     """
+
     def __init__(
-            self,
-            dc: int,
-            host: int = 2,
-            mosi: int = -1,
-            miso: int = -1,
-            sclk: int = -1,
-            cs: int = -1,
-            freq: int = -1,
-            *,
-            tx_only: bool = False,
-            cmd_bits: int = 8,
-            param_bits: int = 8,
-            dc_low_on_data: bool = False,
-            lsb_first: bool = False,
-            cs_high_active: bool = False,
-            spi_mode:int = 0,
-            wp: int = -1,  # Not yet suppported
-            hd: int = -1,  # Not yet supported
-            quad_spi: bool = False,  # Not yet supported
-            sio_mode: bool = False,  # Not yet supported
-            ) -> None:
+        self,
+        dc: int,
+        host: int = 2,
+        mosi: int = -1,
+        miso: int = -1,
+        sclk: int = -1,
+        cs: int = -1,
+        freq: int = -1,
+        *,
+        tx_only: bool = False,
+        cmd_bits: int = 8,
+        param_bits: int = 8,
+        dc_low_on_data: bool = False,
+        lsb_first: bool = False,
+        cs_high_active: bool = False,
+        spi_mode: int = 0,
+        wp: int = -1,  # Not yet suppported
+        hd: int = -1,  # Not yet supported
+        quad_spi: bool = False,  # Not yet supported
+        sio_mode: bool = False,  # Not yet supported
+    ) -> None:
         """
         Initialize the SPI bus with the given parameters.
         """
@@ -147,7 +156,9 @@ class SPIBus(_BaseBus):
         self._cs_inactive: bool = not cs_high_active
 
         self.dc: Pin = Pin(dc, Pin.OUT, value=self._dc_cmd)
-        self.cs: Pin = Pin(cs, Pin.OUT, value=self._cs_inactive) if cs != 0 else lambda val: None
+        self.cs: Pin = (
+            Pin(cs, Pin.OUT, value=self._cs_inactive) if cs != 0 else lambda val: None
+        )
 
         if mosi == -1 and miso == -1 and sclk == -1:
             self.spi: SPI = SPI(
@@ -157,7 +168,7 @@ class SPIBus(_BaseBus):
                 phase=spi_mode & 1,
                 bits=max(cmd_bits, param_bits),
                 firstbit=SPI.LSB if lsb_first else SPI.MSB,
-            )       
+            )
         else:
             self.spi: SPI = SPI(
                 host,
@@ -172,45 +183,37 @@ class SPIBus(_BaseBus):
             )
 
     def tx_color(
-            self,
-            cmd: int,
-            data: memoryview,
-            x_start: int,
-            y_start: int,
-            x_end: int,
-            y_end: int,
-            ) -> None:
+        self,
+        cmd: int,
+        data: memoryview,
+        x_start: int,
+        y_start: int,
+        x_end: int,
+        y_end: int,
+    ) -> None:
         """
         Transmit color data over the SPI bus.
         """
         self.trans_done = False
 
-        if self._rgb565_byte_swap: self.rgb565_byte_swap(data, len(data) // 2)
+        # Swap the color bytes if necessary before writing all at once
+        self.tx_param(cmd, self.swap_bytes(data, len(data) // 2))
 
-        # Write data in chunks of size self.max_transfer_sz (NOT WORKING YET)
-        # for i in range(0, len(data), self.max_transfer_sz):
-        #     chunk = data[i:i+self.max_transfer_sz]
-        #     self.tx_param(cmd, chunk)
-
-        # Write data all at once
-        self.tx_param(cmd, data)
-        
         self.bus_trans_done_cb()
 
     def tx_param(
-            self,
-            cmd: int,
-            data: Optional[memoryview] = None,
-            ) -> None:
+        self,
+        cmd: int,
+        data: Optional[memoryview] = None,
+    ) -> None:
         """
         Transmit parameters over the SPI bus.
         """
-        struct.pack_into('B', self.buf1, 0, cmd)
+        struct.pack_into("B", self.buf1, 0, cmd)
         self.cs(self._cs_active)
         self.dc(self._dc_cmd)
-        print(f"Writing {self.buf1}, {cmd}")
         self.spi.write(self.buf1)
-        if len(data):
+        if data and len(data):
             self.dc(self._dc_data)
             self.spi.write(data)
         self.cs(self._cs_inactive)
