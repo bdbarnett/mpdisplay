f5caf1ae3586d4d66df02b7c3600ebf624819a73
Brad Barnett <bbarnett@gmail.com>
2024-01-03 13:01:07 -0600

    qualia WIP

---
+++ board_configs/qualia/board_config.py
@@ -0,0 +1,77 @@
+""" Qualia with 4.0" 720x720 display """
+
+from lcd_bus import RGBBus
+from busdisplay import BusDisplay
+# from machine import Pin, I2C
+# from cst8xx import CST8XX  # TODO: port to MicroPython from https://github.com/adafruit/Adafruit_CircuitPython_CST8XX
+
+
+display_bus = RGBBus(
+    hsync=41,
+    vsync=42,
+    de=2,
+    disp=-1,
+    pclk=1,
+    data0=11,  #r0
+    data1=10,  #r1
+    data2=9,  #r2
+    data3=46,  #r3
+    data4=3,  #r4
+    data5=48,  #g0
+    data6=47,  #g1
+    data7=21,  #g2
+    data8=14,  #g3
+    data9=13,  #g4
+    data10=12,  #g5
+    data11=40,  #b0
+    data12=39,  #b1
+    data13=38,  #b2
+    data14=0,  #b3
+    data15=45,  #b4
+    freq=16000000,
+    hsync_pulse_width=2,
+    hsync_front_porch=46,
+    hsync_back_porch=44,
+    hsync_idle_low=False,
+    vsync_pulse_width=2,
+    vsync_front_porch=16,
+    vsync_back_porch=18,
+    vsync_idle_low=False,
+    pclk_active_neg=False,  # ?pclk_active_high
+    pclk_idle_high=False,
+    de_idle_high=False,
+    num_fbs=2,
+    bb_size_px=0,
+    bb_inval_cache=False,
+    fb_in_psram=False,
+    no_fb=False,
+    disp_active_low=False,
+    refresh_on_demand=False,
+)
+
+# display_bus.init(720, 720, 16, buffer_size=1_036_800, rgb565_byte_swap=False)
+
+# display_drv = BusDisplay(
+#     display_bus,
+#     width=720,
+#     height=720,
+#     colstart=0,
+#     rowstart=0,
+#     rotation=0,
+#     mirrored=False,
+#     color_depth=16,
+#     bgr=True,
+#     reverse_bytes_in_word=False,
+#     invert=False,
+#     brightness=1.0,
+#     backlight_pin=None,
+#     backlight_on_high=True,
+#     reset_pin=None,
+#     reset_high=True,
+#     power_pin=None,
+#     power_on_high=True,
+# )
+
+# touch_drv = CST8XX(i2c)
+# touch_read_func = touch_drv.touches
+# touch_rotation_table=None
