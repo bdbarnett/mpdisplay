4ac34ef4f869fd361ecd4759fa4dd9993b4a5e41
Brad Barnett <127794626+bdbarnett@users.noreply.github.com>
2024-02-01 15:46:53 -0600

    Update gui_framework.py

--- lib/gui_framework.py
+++ lib/gui_framework.py
@@ -40,38 +40,6 @@ if platform == "esp32":
     except ImportError:
         pass
 
-# Number of colors that have been registered with get_color using idx=None
-registered_colors = 0
-
-def get_color(r, g, b, idx=None):
-    """
-    Get an RGB565 or RGB332 value for a color and optionally register it in the display's LUT.
-    This is a convenience function for using gui_framework WITHOUT Nano-GUI or Micro-GUI.
-    Those packages have their own methods of registering colors.
-
-    :param r: Red component (0-255)
-    :param g: Green component (0-255)
-    :param b: Blue component (0-255)
-    :param idx: Optional index to register the color in the display's LUT (0-15)
-    :return: RGB565 or RGB332 color value, or the index of the registered color
-    """
-    global registered_colors  # Index of the next color to register
-    c = SSD.rgb(r, g, b)  # Convert the color to RGB565 or RGB332
-    if not hasattr(SSD, 'lut'):  # If the ssd doesn't use a LUT in its current mode
-        return c  # Return the color as-is
-    if idx == None:  # If no index was provided
-        if registered_colors < 16:  # If there are fewer than 16 colors already registered
-            idx = registered_colors  # Set the index to the next available index
-            registered_colors += 1  # Increment the index for the next color
-        else:  # If there are already 16 colors registered
-            raise ValueError("16 colors have already been registered")
-    if not 0 <= idx <= 15:  # If the index is out of range
-        raise ValueError('Color numbers must be 0..15')
-    offset = idx << 1  # Multiply by 2 (2 bytes per 16-bit color)
-    SSD.lut[offset] = c & 0xff  # Set the lower 8 bits of the color
-    SSD.lut[offset + 1] = c >> 8  # Set the upper 8 bits of the color
-    return idx  # Return the index of the registered color
-
 @micropython.viper
 def _lcopy8(dest:ptr8, source:ptr8, length:int, swapped:bool, bgr:bool):
     # Convert a line in 8 bit RGB332 format to 16 bit RGB565 format.
@@ -128,11 +96,12 @@ class SSD(framebuf.FrameBuffer):
     rgb = None  # Function to convert r, g, b to a color value
 
     def __init__(self, display_drv, mode):
-        self.palette = BoolPalette(mode)  # a 2-value color palette for rendering monochrome glyphs
         self.display_drv = display_drv
         self.height = display_drv.height
         self.width = display_drv.width
         self.bgr = display_drv.bgr
+        self.palette = BoolPalette(mode)  # a 2-value color palette for rendering monochrome glyphs
+                                          # with ssd.blit(glyph_buf, x, y, key=-1, pallette=ssd.pallette)
 
         # If the display bus is a MicroPython bus (not C) and it has byte swapping enabled,
         # disable it and set a flag so we can swap the bytes as they are put into the buffers
@@ -169,6 +138,7 @@ class SSD(framebuf.FrameBuffer):
             self.show = self._show8
         elif mode == framebuf.GS4_HMSB:
             SSD.lut = bytearray(0x00 for _ in range(32))
+            SSD.registered_colors = 0  # Not used in Nano-GUI or Micro-GUI
             self._linebuf = alloc_buffer(self.width * 2)
             self._buffer = bytearray(self.width * self.height // 2)
             self.show = self._show4
@@ -247,6 +217,35 @@ class SSD(framebuf.FrameBuffer):
         color = SSD._bgr16(r, g, b)
         return (color & 0xff) << 8 | (color & 0xff00) >> 8
 
+    @staticmethod
+    def get_color(r, g, b, idx=None):
+        """
+        Get an RGB565 or RGB332 value for a color and optionally register it in the display's LUT.
+        This is a convenience function for using this framework WITHOUT Nano-GUI or Micro-GUI.
+        Those packages have their own methods of registering colors.
+    
+        :param r: Red component (0-255)
+        :param g: Green component (0-255)
+        :param b: Blue component (0-255)
+        :param idx: Optional index to register the color in the display's LUT (0-15)
+        :return: RGB565 or RGB332 color value or the index of the registered color
+        """
+        c = SSD.rgb(r, g, b)  # Convert the color to RGB565 or RGB332
+        if not hasattr(SSD, 'lut'):  # If the ssd doesn't use a LUT in its current mode
+            return c  # Return the color as-is
+        if idx == None:  # If no index was provided
+            if SSD.registered_colors < 16:  # If there are fewer than 16 colors already registered
+                idx = SSD.registered_colors  # Set the index to the next available index
+                SSD.registered_colors += 1  # Increment the index for the next color
+            else:  # If there are already 16 colors registered
+                raise ValueError("16 colors have already been registered")
+        if not 0 <= idx <= 15:  # If the index is out of range
+            raise ValueError('Color numbers must be 0..15')
+        offset = idx << 1  # Multiply by 2 (2 bytes per 16-bit color)
+        SSD.lut[offset] = c & 0xff  # Set the lower 8 bits of the color
+        SSD.lut[offset + 1] = c >> 8  # Set the upper 8 bits of the color
+        return idx  # Return the index of the registered color
+    
 
 class BoolPalette(framebuf.FrameBuffer):
     # This is a 2-value color palette for rendering monochrome glyphs to color
