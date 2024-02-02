de69ca6760b5f01aca709fbbd1d529c4412ca870
Brad Barnett <bbarnett@gmail.com>
2024-02-02 03:06:41 -0600

    add comments

--- color_setup.py
+++ color_setup.py
@@ -8,8 +8,8 @@ from gui_framework import SSD
 import framebuf
 import board_config
 
-mode = framebuf.GS4_HMSB
-# mode = framebuf.GS8
-# mode = framebuf.RGB565
+mode = framebuf.GS4_HMSB  # 4-bit (16 item) lookup table of 16-bit RGB565 colors; w*h/2 buffer
+# mode = framebuf.GS8  # 256 8-bit RGB332 colors; w*h buffer
+# mode = framebuf.RGB565  # all 65,536 16-bit RGB565 colors; w*h*2 buffer
 
 ssd = SSD(board_config.display_drv, mode)
