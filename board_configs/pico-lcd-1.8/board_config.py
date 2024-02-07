b7282ca1ca2e1693d9afbb09477e1ebfee6dcc68
Brad Barnett <bbarnett@gmail.com>
2024-02-06 23:50:37 -0600

    add pico-lcd-1.8

---
+++ board_configs/pico-lcd-1.8/board_config.py
@@ -0,0 +1,45 @@
+""" Waveshare Pico-LCD-1.8 128x160 ST7735S display """
+
+from lcd_bus import SPIBus
+from st7735r import ST7735R
+
+
+display_bus = SPIBus(
+    dc=8,
+    cs=9,
+    mosi=11,
+    sclk=10,
+    host=1,
+    tx_only=True,
+    freq=20_000_000,
+    spi_mode=0,
+    cmd_bits=8,
+    param_bits=8,
+    lsb_first=False,
+    dc_low_on_data=False,
+    cs_high_active=False,
+)
+
+display_drv = ST7735R(
+    display_bus,
+    width=128,
+    height=160,
+    colstart=2,
+    rowstart=1,
+    rotation=0,
+    mirrored=False,
+    color_depth=16,
+    bgr=True,
+    reverse_bytes_in_word=False,
+    invert=True,
+    brightness=1.0,
+    backlight_pin=13,
+    backlight_on_high=True,
+    reset_pin=12,
+    reset_high=False,
+    power_pin=None,
+    power_on_high=True,
+)
+
+touch_read_func = lambda : None
+touch_rotation_table = None
