4f763118d587b8c1c932f32a0590941f5dff96da
Brad Barnett <bbarnett@gmail.com>
2024-02-09 01:58:35 -0600

    delete _spibus_rp2.py

--- lib/lcd_bus/_spibus_rp2.py
+++
@@ -1,7 +0,0 @@
-from ._spibus import SPIBus as _SPIBus
-
-
-class SPIBus(_SPIBus):
-    def __init__(self, *args, **kwargs):
-        print("Using _spibus_rp2.py")
-        super().__init__(*args, **kwargs)
