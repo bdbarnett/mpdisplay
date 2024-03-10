8318e282987427c762fb7cbcd2e667dc3a57b96f
Brad Barnett <bbarnett@gmail.com>
2024-03-10 14:57:37 -0500

    begin implementing sequential pins / no LUT

--- mpdisplay/lcd_bus/_i80bus.py
+++ mpdisplay/lcd_bus/_i80bus.py
@@ -21,6 +21,10 @@ class I80Bus(BaseBus):
     ESP32, RP2, SAMD and NRF use pin numbers and should work with this driver.
     MIMXRT and STM use pin names and have not been tested.
 
+    If pins are sequential and on the same port, the driver will write data directly to the
+    GPIO registers.  If the pins are not sequential or are on different ports, the driver will
+    use lookup tables to write data to the GPIO registers.
+
     Args:
         dc (int): The pin number for the DC pin.
         cs (int): The pin number for the CS pin.
@@ -74,6 +78,10 @@ class I80Bus(BaseBus):
         if len(pins) != 8:
             raise ValueError("bus width must be 8")
 
+        # Set the data pins as outputs
+        for pin in pins:
+            Pin(pin, Pin.OUT)
+
         # Setup the control pins
         self._dc_data: bool = bool(dc_data_level)
         self._dc_cmd: bool = not self._dc_data
@@ -89,12 +97,21 @@ class I80Bus(BaseBus):
         self._wr_inactive: bool = not self._wr_active  # not used in this class, may be used in a subclass
         self.wr: Pin = Pin(wr, Pin.OUT, value=pclk_active_neg)
 
-        self._setup(pins, wr, freq)
+        # Check to see if pins are sequential and all on the same port:
+        if all(pins[i] + 1 == pins[i + 1] for i in range(len(pins) - 1)) and \
+                pins[0] // 32 == pins[-1] // 32:
+            # Use sequential mode
+            self._setup_seq(pins, wr, freq)
+        else:
+            # Use LUT mode
+            self._setup_lut(pins, wr, freq)
 
-    def _setup(self, pins: list[int], wr: int, freq: int) -> None:
+    def _setup_lut(self, pins: list[int], wr: int, freq: int) -> None:
         """
         Setup lookup tables and pin data for the _write method.
         """
+        print("Using LUT mode")
+        self._write = self._write_lut
         from ._gpio_registers import GPIO_SET_CLR_REGISTERS
 
         # Use the GPIO_SET_CLR_REGISTERS class to get the register addresses and masks
@@ -113,10 +130,7 @@ class I80Bus(BaseBus):
         # Get the register addresses for the write pin
         self._wr_reg, self._wr_not_reg = gpio.get_set_clr_regs(wr, self._wr_active)
 
-        # Set the data pins as outputs
-        for pin in pins:
-            Pin(pin, Pin.OUT)
-
+        # Setup the data for pin_data and the lookup tables
         self._lookup_tables = []  # list of memoryview lookup tables
         pin_masks = []  # list of 32-bit pin masks
         regsA = []  # list of set registers if _is_32bit else set/reset registers for lower 16 pins
@@ -166,8 +180,51 @@ class I80Bus(BaseBus):
             pin_data[i * 4 + 3] = addressof(self._lookup_tables[i])
         self._pin_data = memoryview(pin_data)
 
+    def _setup_seq(self, pins: list[int], wr: int, freq: int) -> None:
+        """
+        Setup pin data for the _write method.
+        """
+        print("Using sequential mode")
+        self._write = self._write_seq
+        from ._gpio_registers import GPIO_SET_CLR_REGISTERS
+
+        # Use the GPIO_SET_CLR_REGISTERS class to get the register addresses and masks
+        # for the data pins and wr pin and to determine the number of pins per port for
+        # _write().  Implemented as a local variable to allow the instance to be garbage
+        # collected after this function has completed.
+        gpio = GPIO_SET_CLR_REGISTERS()
+
+        # If self._is_32bit is True the _write method will use a 32-bit set and a 32-bit
+        # clear register.  Otherwise, the _write method will use set_reset registers
+        # which use the lower 16 bits for set and the upper 16 bits for clear.
+        self._is_32bit = True if gpio.pins_per_port == 32 else False
+
+        # Get the masks for the write pin
+        self._wr_mask, self._wr_not_mask = gpio.get_set_clr_masks(wr, self._wr_active)
+        # Get the register addresses for the write pin
+        self._wr_reg, self._wr_not_reg = gpio.get_set_clr_regs(wr, self._wr_active)
+
+        # Setup the data for pin_data
+        start_pin = pins[0]
+        pin_mask = sum([1 << (p - start_pin) for p in pins]) << (start_pin % 32)
+        if self._is_32bit:
+            regA = gpio.set_reg(start_pin)
+            regB = gpio.clr_reg(start_pin)
+        else:
+            regA = gpio.set_reset_reg(start_pin)
+            regB = gpio.set_reset_reg(start_pin + 16)
+        shift = start_pin % 32
+
+        # save all settings in an array pin_data for use in viper
+        pin_data = array("I", [0] * 4)
+        pin_data[0] = pin_mask
+        pin_data[1] = regA
+        pin_data[2] = regB
+        pin_data[3] = shift
+        self._pin_data = memoryview(pin_data)
+
     @micropython.viper
-    def _write(self, data: ptr8, length: int):
+    def _write_lut(self, data: ptr8, length: int):
         wr_not_reg = ptr32(self._wr_not_reg)
         wr_not_mask = int(self._wr_not_mask)
         wr_reg = ptr32(self._wr_reg)
@@ -195,5 +252,35 @@ class I80Bus(BaseBus):
                         else:
                             regA[0] = (tx_value & 0xFFFF) | ((tx_value ^ pin_mask) << 16)
                             regB[0] = (tx_value >> 16) | ((tx_value ^ pin_mask) & 0xFFFF0000)
+                last = val
+            wr_reg[0] = wr_mask  # WR Active
+
+    @micropython.viper
+    def _write_seq(self, data: ptr8, length: int):
+        wr_not_reg = ptr32(self._wr_not_reg)
+        wr_not_mask = int(self._wr_not_mask)
+        wr_reg = ptr32(self._wr_reg)
+        wr_mask = int(self._wr_mask)
+        is_32bit = bool(self._is_32bit)
+        pin_data = ptr32(self._pin_data)
+
+        pin_mask = pin_data[0]
+        regA = ptr32(pin_data[1])
+        regB = ptr32(pin_data[2])
+        shift = pin_data[3]
+
+        last: int = -1
+        for i in range(length):
+            wr_not_reg[0] = wr_not_mask  # WR Inactive
+            val = data[i]
+            if val != last:
+                tx_value: int = val << shift
+                # if _is_32bit:  # TODO:  Not working yet. Assume 32-bit for now.
+                if True:
+                    regA[0] = tx_value
+                    regB[0] = tx_value ^ pin_mask
+                else:
+                    regA[0] = (tx_value & 0xFFFF) | ((tx_value ^ pin_mask) << 16)
+                    regB[0] = (tx_value >> 16) | ((tx_value ^ pin_mask) & 0xFFFF0000)
                 last = val
             wr_reg[0] = wr_mask  # WR Active
