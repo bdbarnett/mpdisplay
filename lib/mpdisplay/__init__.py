f10de3760caec0d797317e500ad5e9b8454f08ba
Brad Barnett <bbarnett@gmail.com>
2024-04-07 08:46:06 -0500

    add circuitpython

--- lib/mpdisplay/__init__.py
+++ lib/mpdisplay/__init__.py
@@ -2,10 +2,12 @@ from sys import implementation, platform
 
 if implementation.name == "cpython":
     from ._pgdisplay import *
+elif implementation.name == "circuitpython":
+    from ._busdisplay import *
 elif implementation.name == "micropython":
     if platform == "linux":
         from ._sdl2display import *
     else:
         from ._busdisplay import *
 else:
-    raise ImportError("Unsupported Python implementation")
+    raise ImportError("unsupported Python implementation")
