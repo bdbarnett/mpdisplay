95653f1579045d1d9809b0cf517f8669faa94c76
Brad Barnett <bbarnett@gmail.com>
2024-04-08 23:34:59 -0500

    add path.py utility

---
+++ utils/path.py
@@ -0,0 +1,8 @@
+import sys
+
+
+### Change the path below to the path to your MicroPython lib folder.
+### To run this command when you run Python, type:
+###
+###     python -i path.py
+sys.path.append('lib')
