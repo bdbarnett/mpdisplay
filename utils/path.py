08eaa7610882cf360b18d2e27a3f2341db67c924
Brad Barnett <bbarnett@gmail.com>
2024-04-12 18:51:49 -0500

    move path.py to tft_graphics

--- utils/path.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-
-### Change the path below to the path to your MicroPython lib folder.
-### To run this command when you run Python, type:
-###
-###     python -i path.py
-sys.path.extend(['lib', 'romfonts'])
