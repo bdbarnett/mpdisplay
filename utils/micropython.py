45e46aeae6b0c7333cff7a397f55d0487f14b209
Brad Barnett <bbarnett@gmail.com>
2024-04-15 14:31:46 -0500

    edit

--- utils/micropython.py
+++ utils/micropython.py
@@ -3,11 +3,5 @@ micropython.py - A module to prevent errors when running applications
 written for both CPython and MicroPython.  Doesn't (necessarily) allow
 the functions to run, but prevents them from throwing errors when loaded.
 """
-const = lambda x: x
-
-def viper(func):
-    return func
-
-def native(func):
-    return func
+const = viper = native = lambda x: x
 
