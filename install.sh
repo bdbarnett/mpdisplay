2f09a94a6328ef2f6fc003bc66ec427c4ceb46ec
Brad Barnett <127794626+bdbarnett@users.noreply.github.com>
2024-04-26 10:13:19 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,8 +69,8 @@ EXE=python3  # micropython, python3 or python
 
 ##################### Optional: set these variables #####################################
 
-BOARD_CONFIG=mpdisplay/board_configs/desktop/board_config.py
-LAUNCH=testris
+BOARD_CONFIG=mpdisplay/board_configs/desktop/board_config.py  # with .py extension
+LAUNCH=testris                                                # without .py extension
 
 ######################## Download the repositories ###############################
 
