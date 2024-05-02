bb6bc2778f2aeec45bd4956156d66cc2a279e41f
Brad Barnett <bbarnett@gmail.com>
2024-05-02 01:29:42 -0500

    install.py

--- install.py
+++ install.py
@@ -5,6 +5,9 @@ TARGET = "."
 # Install your board_config
 mip.install("github:bdbarnett/mpdisplay/board_configs/desktop", target=TARGET)
 
+# Install lcd_bus.  Comment out this line if you don't need it.
+mip.install("github:bdbarnett/lcd_bus/lcd_bus", target=TARGET)
+
 # Install the mpdisplay library
 mip.install("github:bdbarnett/mpdisplay/mpdisplay", target=TARGET)
 
