133b7c185588a9e05c500c78971b156f4b01b96d
bbarnett822 <bbarnett@gmail.com>
2024-05-02 12:53:59 -0500

    path.py

--- path.py
+++ path.py
@@ -1,10 +1,40 @@
-import sys
+'''
+path.py
+To run this command when you launch Python, type the following, substituting 'python' with the name
+of your Python executable, such as 'python3' or 'micropython':
 
+    python -i path.py
 
-### Change the path below to the path to your *Python lib folder.
-### To run this command when you run Python, type:
-###
-###     python -i path.py
-if not 'lib' in sys.path:
-    sys.path.append('lib')
-sys.path.extend(['drivers', 'romfonts', 'examples'])
+On microcontrollers, you may include it in your boot.py, main.py or code.py, whichever is appropriate:
+    
+    import path
+
+Edit the 'directories' tuple to include the directories you want to add to the path.
+Only directories that already exist on the filesystem will be added to the path.
+Does not work for nested directories.
+'''
+def update():
+    # Edit this list to include the directories you want to add to the path.
+    directories = ('lib', 'drivers', 'examples', 'fonts', 'romfonts')
+
+    import sys
+    import os
+
+    # Get a list of directories on the filesystem.
+    if hasattr(os, 'listdir'):
+        dirlist = os.listdir()
+    else:
+        dirlist = [x[0] for x in os.ilistdir()]
+
+    # Check to see if each directory is on the filesystem and if so, add it to the path.
+    prefix = 'path.py:  Added'
+    postfix = ''
+    for directory in directories:
+        if directory in dirlist and not directory in sys.path:
+            sys.path.append(directory)
+            print(f"{prefix} {directory}", end='')
+            prefix = ','
+            postfix = ' to the path.\n'
+    print(postfix, end='')
+
+update()
