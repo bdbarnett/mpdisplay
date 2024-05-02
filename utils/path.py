e6783e7fc9c2c7269ad74e98d225b69342581d97
Brad Barnett <bbarnett@gmail.com>
2024-05-01 23:33:29 -0500

    path.py

---
+++ utils/path.py
@@ -0,0 +1,10 @@
+import sys
+
+
+### Change the path below to the path to your *Python lib folder.
+### To run this command when you run Python, type:
+###
+###     python -i path.py
+if not 'lib' in sys.path:
+    sys.path.append('lib')
+sys.path.extend(['romfonts', 'examples'])
