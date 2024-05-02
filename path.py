df053a791b9eb5c43e272f8d2132fd34f2cbcb74
Brad Barnett <127794626+bdbarnett@users.noreply.github.com>
2024-05-02 12:56:07 -0500

    Update path.py

--- path.py
+++ path.py
@@ -1,7 +1,7 @@
 '''
 path.py
 To run this command when you launch *Python, type the following, substituting 'python' with the name
-of your Python executable, such as 'python3' or 'micropython':
+of your *Python executable, such as 'python3' or 'micropython':
 
     python -i path.py
 
