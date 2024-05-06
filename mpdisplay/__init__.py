db5d8a5e4e7001241e03a063b2cfec8409baa6a0
Brad Barnett <bbarnett@gmail.com>
2024-05-05 20:05:40 -0500

    add ChromeOS penguin

--- mpdisplay/__init__.py
+++ mpdisplay/__init__.py
@@ -1,22 +1,25 @@
 # SPDX-FileCopyrightText: 2024 Brad Barnett
 #
 # SPDX-License-Identifier: MIT
-from sys import implementation, platform
+import sys
 from ._basedisplay import _BaseDisplay
 from ._devices import Devices
 from ._events import Events
 from ._keys import Keys
 
 
-if implementation.name == "cpython":
-#     from ._pgdisplay import PGDisplay as DesktopDisplay, PGEvents as DesktopEvents
-    from ._sdl2display import SDL2Display as DesktopDisplay, SDL2Events as DesktopEvents
-elif implementation.name == "circuitpython":
-    from ._busdisplay import BusDisplay
-elif implementation.name == "micropython":
-    if platform == "linux":
+if sys.implementation.name == "cpython":
+    import platform
+    if platform.uname().node == "penguin":  # ChromeOS
+        from ._pgdisplay import PGDisplay as DesktopDisplay, PGEvents as DesktopEvents
+    else:
+        from ._sdl2display import SDL2Display as DesktopDisplay, SDL2Events as DesktopEvents
+elif sys.implementation.name == "micropython":
+    if sys.platform == "linux":
         from ._sdl2display import SDL2Display as DesktopDisplay, SDL2Events as DesktopEvents
     else:
         from ._busdisplay import BusDisplay
+elif sys.implementation.name == "circuitpython":
+    from ._busdisplay import BusDisplay
 else:
     raise ImportError("unsupported Python implementation")
