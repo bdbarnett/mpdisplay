cce699b34954ed56c2a96fc4f25d0a9ecef4a726
Brad Barnett <bbarnett@gmail.com>
2024-05-05 21:40:42 -0500

    import flowchart

--- mpdisplay/__init__.py
+++ mpdisplay/__init__.py
@@ -7,16 +7,43 @@ from ._devices import Devices
 from ._events import Events
 from ._keys import Keys
 
+"""
+Import flowchart:
+
+Is it CPython?
+    Yes, is it ChromeOS?
+        Yes, import PGDisplay
+        No, try to import SDL2Display
+            If fail, print error and import PGDisplay
+Is it MicroPython?
+    Yes, is it Linux?
+        Yes, try to import SDL2Display
+            If fail, print error and import PGDisplay
+        No, import BusDisplay
+Is it CircuitPython?
+    Yes, import BusDisplay
+Else, raise ImportError
+"""
 
 if sys.implementation.name == "cpython":
     import platform
     if platform.uname().node == "penguin":  # ChromeOS
         from ._pgdisplay import PGDisplay as DesktopDisplay, PGEvents as DesktopEvents
     else:
-        from ._sdl2display import SDL2Display as DesktopDisplay, SDL2Events as DesktopEvents
+        try:
+            from ._sdl2display import SDL2Display as DesktopDisplay, SDL2Events as DesktopEvents
+        except Exception as e:
+            print(f"MPDisplay: Error loading SDL2Display -- {e}")
+            print("    Falling back to PGDisplay")
+            from ._pgdisplay import PGDisplay as DesktopDisplay, PGEvents as DesktopEvents
 elif sys.implementation.name == "micropython":
     if sys.platform == "linux":
-        from ._sdl2display import SDL2Display as DesktopDisplay, SDL2Events as DesktopEvents
+        try:
+            from ._sdl2display import SDL2Display as DesktopDisplay, SDL2Events as DesktopEvents
+        except Exception as e:
+            print(f"MPDisplay:  Error loading SDL2Display -- {e}")
+            print("    Falling back to PGDisplay")
+            from ._pgdisplay import PGDisplay as DesktopDisplay, PGEvents as DesktopEvents
     else:
         from ._busdisplay import BusDisplay
 elif sys.implementation.name == "circuitpython":
