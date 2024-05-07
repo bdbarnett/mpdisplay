c0cbcaa61b3b11076e8dd8c5e5b19dd7d7050af5
Brad Barnett <bbarnett@gmail.com>
2024-05-06 23:10:50 -0500

    remove board_config

--- install.py
+++ install.py
@@ -1,10 +1,7 @@
 import mip
 
-TARGET = "."
 
-# Install your board_config.  Modify the path to match your board_config.
-mip.install("github:bdbarnett/mpdisplay/board_configs/desktop", target=TARGET)
-print("")
+TARGET = "."
 
 # Install sdl2_lib.  Comment out this line if you are only using lcd_bus.
 mip.install("github:bdbarnett/sdl2_lib", target=TARGET)
@@ -40,3 +37,12 @@ mip.install("github:bdbarnett/timer", target=TARGET)
 print("")
 
 mip.install("github:bdbarnett/testris", target=TARGET)
+print("")
+
+
+# Install your board_config.  Modify the path to match your board_config.
+print("You must now install your board_config.  Type the following, substituting `desktop` with your board",
+"""
+    mip.install("github:bdbarnett/mpdisplay/board_configs/desktop", target=".")
+""", "\n")
+
