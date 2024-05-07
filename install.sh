9415c95b2b5fff80bfa7efe8f0c10a5b7264f359
Brad Barnett <bbarnett@gmail.com>
2024-05-07 03:01:18 -0500

    install.sh

--- install.sh
+++ install.sh
@@ -74,7 +74,8 @@ EXE=python3  # micropython, python3 or python
 ##################### Optional: set these variables #####################################
 
 BOARD_CONFIG=mpdisplay/board_configs/desktop/board_config.py  # with .py extension
-LAUNCH=mpdisplay_paint  # without .py extension
+#LAUNCH=mpdisplay_paint  # without .py extension
+LAUNCH=romfont_simpletest  # without .py extension
 
 
 ######################## Download the repositories ###############################
