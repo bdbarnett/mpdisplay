3e4d3b969dd6f5a7e8ed5ef7791392016e2207af
Brad Barnett <bbarnett@gmail.com>
2024-05-27 06:29:59 -0500

    palettes

--- examples/palette_cube.py
+++ examples/palette_cube.py
@@ -7,19 +7,23 @@ if display_drv.requires_byte_swap:
 else:
     needs_swap = False
 
-palette = display_drv.get_palette(name="cube", size=3, color_depth=16, swapped=needs_swap)
+palette = display_drv.get_palette(name="cube", size=5, color_depth=16, swapped=needs_swap)
+# palette = display_drv.get_palette(name="wheel", length=522, color_depth=16, swapped=needs_swap)
 
-line_height = 18
-
+line_height = 20
+last_line = display_drv.height - line_height
+scroll = 0
 y = 0
 def main():
-    global y
+    global y, scroll
     for index, color in enumerate(palette):
+        if y - scroll - last_line > 0:
+            scroll = (y - last_line) % display_drv.height
+            display_drv.vscsad(scroll)
         name = f"{index} - {palette.color_name(index)}"
-        if y + line_height >= display_drv.height:
-            display_drv.vscsad((y + line_height) % display_drv.height)
+        text_color = palette.WHITE if palette.brightness(index) < 0.4 else palette.BLACK
         display_drv.fill_rect(0, y % display_drv.height, display_drv.width, line_height, color)
-        display_drv.btext(name, 0, (y + 1) % display_drv.height, ~color)
+        display_drv.btext(name, 2, (y + 2) % display_drv.height, text_color)
         y += line_height
 
 
--- lib/primitives/palettes/_palette.py
+++ lib/primitives/palettes/_palette.py
@@ -3,133 +3,133 @@
 #
 # SPDX-License-Identifier: MIT
 
-
+# 125 colors with only 0x00, 0x40, 0x80, 0xC0, 0xFF as RGB values
 NAMES = {
     0x000000: "Black",
-    0x000040: "Navy Blue",
-    0x000080: "Dark Blue",
-    0x0000C0: "Cobalt Blue",
+    0x000040: "Midnight Express",
+    0x000080: "Navy",
+    0x0000C0: "Medium Blue",
     0x0000FF: "Blue",
-    0x004000: "Dark Green",
-    0x004040: "Teal",
-    0x004080: "Steel Blue",
-    0x0040C0: "Deep Sky Blue",
-    0x0040FF: "Dodger Blue",
+    0x004000: "Myrtle",
+    0x004040: "Cyprus",
+    0x004080: "Dark Cerulean",
+    0x0040C0: "Cobalt",
+    0x0040FF: "Vivid Navy",
     0x008000: "Green",
-    0x008040: "Medium Sea Green",
-    0x008080: "Dark Cyan",
-    0x0080C0: "Light Sea Green",
-    0x0080FF: "Deep Sky Blue",
-    0x00C000: "Lime Green",
-    0x00C040: "Spring Green",
-    0x00C080: "Medium Spring Green",
-    0x00C0C0: "Aquamarine",
-    0x00C0FF: "Turquoise",
+    0x008040: "Dark Spring Green",
+    0x008080: "Teal",
+    0x0080C0: "Cerulean",
+    0x0080FF: "Azure",
+    0x00C000: "Islamic Green",
+    0x00C040: "Dark Pastel Green",
+    0x00C080: "Faux-Green",
+    0x00C0C0: "Iris Blue",
+    0x00C0FF: "Deep Sky Blue",
     0x00FF00: "Lime",
-    0x00FF40: "Lawn Green",
-    0x00FF80: "Chartreuse",
-    0x00FFC0: "Pale Green",
+    0x00FF40: "Malachite",
+    0x00FF80: "Spring Green",
+    0x00FFC0: "Medium Spring Green",
     0x00FFFF: "Cyan",
-    0x400000: "Maroon",
-    0x400040: "Purple",
+    0x400000: "Seal Brown",
+    0x400040: "Deep Purple",
     0x400080: "Indigo",
-    0x4000C0: "Blue Violet",
-    0x4000FF: "Medium Slate Blue",
-    0x404000: "Olive",
-    0x404040: "Dim Gray",
-    0x404080: "Slate Blue",
-    0x4040C0: "Royal Blue",
-    0x4040FF: "Medium Blue",
-    0x408000: "Dark Olive Green",
-    0x408040: "Sea Green",
-    0x408080: "Cadet Blue",
-    0x4080C0: "Cornflower Blue",
-    0x4080FF: "Steel Blue",
-    0x40C000: "Yellow Green",
-    0x40C040: "Medium Aquamarine",
+    0x4000C0: "Purple Heart",
+    0x4000FF: "Han Purple",
+    0x404000: "Turtle Green",
+    0x404040: "Dark Gray",
+    0x404080: "Jackson's Purple",
+    0x4040C0: "Free Speech Blue",
+    0x4040FF: "Neon Blue",
+    0x408000: "Bilbao",
+    0x408040: "Japanese Laurel",
+    0x408080: "Paradiso",
+    0x4080C0: "Curious Blue",
+    0x4080FF: "Royal Blue",
+    0x40C000: "Strong Green",
+    0x40C040: "Lime Green",
     0x40C080: "Medium Sea Green",
-    0x40C0C0: "Light Blue",
-    0x40C0FF: "Sky Blue",
-    0x40FF00: "Lime",
-    0x40FF40: "Lawn Green",
-    0x40FF80: "Spring Green",
-    0x40FFC0: "Pale Green",
-    0x40FFFF: "Aquamarine",
-    0x800000: "Red",
-    0x800040: "Deep Pink",
+    0x40C0C0: "Medium Turquoise",
+    0x40C0FF: "Summer Sky",
+    0x40FF00: "Harlequin",
+    0x40FF40: "Lime Green",
+    0x40FF80: "Screamin' Green",
+    0x40FFC0: "Turquoise",
+    0x40FFFF: "Baby Blue",
+    0x800000: "Maroon",
+    0x800040: "Tyrian Purple",
     0x800080: "Purple",
     0x8000C0: "Dark Violet",
-    0x8000FF: "Dark Orchid",
-    0x804000: "Saddle Brown",
-    0x804040: "Rosy Brown",
-    0x804080: "Medium Orchid",
-    0x8040C0: "Medium Purple",
-    0x8040FF: "Slate Blue",
+    0x8000FF: "Violet",
+    0x804000: "Brown",
+    0x804040: "Red Robin",
+    0x804080: "Eminence",
+    0x8040C0: "Dark Orchid",
+    0x8040FF: "Blue Violet",
     0x808000: "Olive",
-    0x808040: "Dark Khaki",
-    0x808080: "Gray",
-    0x8080C0: "Light Slate Gray",
-    0x8080FF: "Light Steel Blue",
-    0x80C000: "Yellow Green",
-    0x80C040: "Chartreuse",
-    0x80C080: "Pale Green",
-    0x80C0C0: "Light Green",
-    0x80C0FF: "Pale Turquoise",
-    0x80FF00: "Lime",
-    0x80FF40: "Spring Green",
+    0x808040: "Wasabi",
+    0x808080: "Grey",
+    0x8080C0: "Moody Blue",
+    0x8080FF: "Light Blue",
+    0x80C000: "Citrus",
+    0x80C040: "Mantis",
+    0x80C080: "De York",
+    0x80C0C0: "Glacier",
+    0x80C0FF: "Maya Blue",
+    0x80FF00: "Chartreuse",
+    0x80FF40: "Green Yellow",
     0x80FF80: "Light Green",
-    0x80FFC0: "Medium Spring Green",
-    0x80FFFF: "Pale Turquoise",
-    0xC00000: "Dark Red",
-    0xC00040: "Medium Violet Red",
-    0xC00080: "Crimson",
-    0xC000C0: "Magenta",
-    0xC000FF: "Deep Pink",
-    0xC04000: "Orange Red",
-    0xC04040: "Tomato",
-    0xC04080: "Hot Pink",
-    0xC040C0: "Deep Pink",
-    0xC040FF: "Orchid",
-    0xC08000: "Dark Orange",
-    0xC08040: "Peru",
-    0xC08080: "Sandy Brown",
-    0xC080C0: "Light Coral",
-    0xC080FF: "Medium Orchid",
-    0xC0C000: "Gold",
-    0xC0C040: "Khaki",
-    0xC0C080: "Dark Khaki",
-    0xC0C0C0: "Silver",
-    0xC0C0FF: "Light Steel Blue",
-    0xC0FF00: "Chartreuse",
-    0xC0FF40: "Lawn Green",
-    0xC0FF80: "Green Yellow",
-    0xC0FFC0: "Pale Green",
+    0x80FFC0: "Aquamarine",
+    0x80FFFF: "Light Cyan",
+    0xC00000: "Free Speech Red",
+    0xC00040: "Cardinal",
+    0xC00080: "Medium Violet Red",
+    0xC000C0: "Deep Magenta",
+    0xC000FF: "Electric Purple",
+    0xC04000: "Rust",
+    0xC04040: "Chestnut",
+    0xC04080: "Medium Red Violet",
+    0xC040C0: "Fuchsia",
+    0xC040FF: "Medium Orchid",
+    0xC08000: "Dark Goldenrod",
+    0xC08040: "Brandy Punch",
+    0xC08080: "Brandy Rose",
+    0xC080C0: "London Hue",
+    0xC080FF: "Heliotrope",
+    0xC0C000: "La Rioja",
+    0xC0C040: "Celery",
+    0xC0C080: "Pine Glade",
+    0xC0C0C0: "Light Grey",
+    0xC0C0FF: "Lavender Blue",
+    0xC0FF00: "Electric Lime",
+    0xC0FF40: "Green Yellow",
+    0xC0FF80: "Sulu",
+    0xC0FFC0: "Granny Apple",
     0xC0FFFF: "Pale Turquoise",
     0xFF0000: "Red",
-    0xFF0040: "Deep Pink",
-    0xFF0080: "Hot Pink",
-    0xFF00C0: "Magenta",
-    0xFF00FF: "Fuchsia",
-    0xFF4000: "Orange Red",
-    0xFF4040: "Tomato",
+    0xFF0040: "Torch Red",
+    0xFF0080: "Rose",
+    0xFF00C0: "Hot Magenta",
+    0xFF00FF: "Magenta",
+    0xFF4000: "Deep Orange",
+    0xFF4040: "Coral Red",
     0xFF4080: "Violet Red",
-    0xFF40C0: "Orchid",
-    0xFF40FF: "Fuchsia",
-    0xFF8000: "Dark Orange",
+    0xFF40C0: "Razzle Dazzle Rose",
+    0xFF40FF: "Pink Flamingo",
+    0xFF8000: "Orange",
     0xFF8040: "Coral",
     0xFF8080: "Light Coral",
-    0xFF80C0: "Medium Orchid",
+    0xFF80C0: "Persian Pink",
     0xFF80FF: "Pink",
-    0xFFC000: "Gold",
-    0xFFC040: "Light Goldenrod",
-    0xFFC080: "Peach Puff",
-    0xFFC0C0: "Light Pink",
-    0xFFC0FF: "Lavender Blush",
+    0xFFC000: "Amber",
+    0xFFC040: "Supernova",
+    0xFFC080: "Macaroni and Cheese",
+    0xFFC0C0: "Your Pink",
+    0xFFC0FF: "Snuff",
     0xFFFF00: "Yellow",
-    0xFFFF40: "Light Yellow",
-    0xFFFF80: "Pale Goldenrod",
-    0xFFFFC0: "Lemon Chiffon",
-    0xFFFFFF: "White",
+    0xFFFF40: "Paris Daisy",
+    0xFFFF80: "Light Yellow",
+    0xFFFFC0: "Cumulus",
+    0xFFFFFF: "White"
 }
 
 
@@ -202,6 +202,14 @@ class Palette:
         if isinstance(r, (tuple, list)):
             r, g, b = r
         return NAMES.get(r << 16 | g << 8 | b, f"#{r:02X}{g:02X}{b:02X}")
+    
+    def luminance(self, index):
+        r, g, b = self._get_rgb(index)
+        return 0.299 * r + 0.587 * g + 0.114 * b
+    
+    def brightness(self, index):
+        r, g, b = self._get_rgb(index)
+        return (r + g + b) / 3 / 255
 
     def _get_rgb(self, index):
         raise NotImplementedError("Subclasses must implement this method")
@@ -219,56 +227,3 @@ class MappedPalette(Palette):
     def _get_rgb(self, index):
         r, g, b = self._color_map[index*3:index*3+3]
         return r, g, b
-
-"""
-
-    def _define_named_colors(self):
-        # Comments show the names from the "12 major color wheel colors".
-        # The second color name in the comments is the palette family from.
-        # Material Design.  An asterisk denote the color name exists in EGA.
-
-        # Monochrome colors with only 0, 127 and 255 as RGB values:
-        self.BLACK = self.color565(0, 0, 0)  # *
-        self.WHITE = self.color565(255, 255, 255)  # *
-        self.GREY = self.color565(127, 127, 127)   # (Grey)
-
-        # The 12 major color wheel colors:
-        self.RED = self.color565(255, 0, 0)  # * Red (Red)
-        self.ORANGE = self.color565(255, 127, 0)  # Orange (Orange)
-        self.YELLOW = self.color565(255, 255, 0)  # * Yellow (Yellow)
-        self.LIME = self.color565(127, 255, 0)  # Chartreuse (Lime)
-        self.GREEN = self.color565(0, 255, 0)  # * Green (Green)
-        self.SPRING_GREEN = self.color565(0, 255, 127)  # Spring Green
-        self.CYAN = self.color565(0, 255, 255)  # * Cyan (Cyan)
-        self.AZURE = self.color565(0, 127, 255)  # Azure
-        self.BLUE = self.color565(0, 0, 255)  # * Blue (Blue)
-        self.INDIGO = self.color565(127, 0, 255)  # Violet (Indigo)
-        self.MAGENTA = self.color565(255, 0, 255)  # * Magenta
-        self.ROSE = self.color565(255, 0, 127)  # Rose
-
-        # The other 12 colors that have only 0, 127 and 255 as RGB values:
-        self.TEAL = self.color565(0, 127, 127)  # (Teal)
-        self.PURPLE = self.color565(127, 0, 127)  # (Purple)
-        self.PINK = self.color565(255, 127, 255)  # (Pink)
-        self.LIGHT_GREEN = self.color565(127, 255, 127)  # * (Light Green)
-        self.LIGHT_BLUE = self.color565(127, 127, 255)  # * (Light Blue)
-        self.LIGHT_CYAN = self.color565(127, 255, 255)  # * Light Cyan
-        self.LIGHT_YELLOW = self.color565(255, 255, 127)  # Light Yellow
-        self.DARK_RED = self.color565(127, 0, 0)  # Dark Red
-        self.DARK_BLUE = self.color565(0, 0, 127)  # Dark Blue
-        self.SKY_BLUE = self.color565(0, 127, 255)  # Sky Blue
-        self.OLIVE = self.color565(127, 127, 0)  # Olive
-        self.SALMON = self.color565(255, 127, 127)  # Salmon
-
-        # Other colors rounding out the 32 named colors:
-        self.BROWN = self.color565(127, 63, 0)  # * (Brown)
-        self.LIGHT_GREY = self.color565(191, 191, 191) # *
-        self.DARK_GREY = self.color565(63, 63, 63) # *
-        self.AMBER = self.color565(255, 191, 0)  # (Amber)
-        self.BLUE_GREY = self.color565(63, 95, 127)  # (Blue Grey)
-
-        # EGA colors that aren't defined:
-        self.LIGHT_RED = self.color565(255, 63, 63)
-        self.LIGHT_MAGENTA = self.color565(255, 63, 255)
-
-"""
