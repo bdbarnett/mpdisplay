1ffd5525c4b3c5b5518176fc26cd189a1fb16d6b
Brad Barnett <bbarnett@gmail.com>
2024-06-07 13:16:03 -0500

    add 8 and 4 bit color_depth

--- lib/palettes/_palette.py
+++ lib/palettes/_palette.py
@@ -24,6 +24,10 @@ class Palette:
         for color, name in self._names.items():
             if self._color_depth == 16:
                 color = self.color565(color)
+            elif self._color_depth == 8:
+                color = self.color332(color)
+            elif self._color_depth == 4:
+                color = list(self._names.keys()).index(color)
             setattr(self, name.replace(" ", "_").upper(), color)
 
     @property
@@ -45,10 +49,12 @@ class Palette:
                 return self._cache[index]
     
         r, g, b = self._get_rgb(index)
-        if self._color_depth == 24:
+        if self._color_depth == 24 or self._color_depth == 4:
             return r << 16 | g << 8 | b
         elif self._color_depth == 16:
             return self.color565(r, g, b)
+        elif self._color_depth == 8:
+            return self.color332(r, g, b)
         raise ValueError("Invalid color depth")
 
     def _normalize(self, index):
@@ -71,6 +77,18 @@ class Palette:
             return (color & 0xFF) << 8 | (color & 0xFF00) >> 8 
         else:
             return color
+
+    def color332(self, r, g=None, b=None):
+        # Convert r, g, b to 8-bit
+        if isinstance(r, (tuple, list)):
+            # r is a tuple or list
+            r, g, b = r
+        elif g is None:
+            # r is a 24-bit color
+            r, g, b = r >> 16 & 0xFF, r >> 8 & 0xFF, r & 0xFF
+
+        color = (r & 0xE0) | (g & 0xE0) >> 3 | (b & 0xC0) >> 6
+        return color
 
     def color_name(self, index):
         return self.rgb_name(self._get_rgb(self._normalize(index)))
