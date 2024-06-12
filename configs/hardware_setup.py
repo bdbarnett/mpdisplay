bff11af22babfdc2e886eacd52450b52667b8a3e
bdbarnett <brad.barnett@outlook.com>
2024-06-12 12:10:56 -0500

    default to RGB565 format

--- configs/hardware_setup.py
+++ configs/hardware_setup.py
@@ -12,9 +12,9 @@ from displaybuf import DisplayBuffer as SSD
 from board_config import display_drv
 from mpdisplay import Events
 
-format = SSD.GS4_HMSB  # 4-bit (16 item) lookup table of 16-bit RGB565 colors; w*h/2 buffer
+# format = SSD.GS4_HMSB  # 4-bit (16 item) lookup table of 16-bit RGB565 colors; w*h/2 buffer
 # format = SSD.GS8  # 256 8-bit RGB332 colors; w*h buffer
-# format = SSD.RGB565  # all 65,536 16-bit RGB565 colors; w*h*2 buffer
+format = SSD.RGB565  # all 65,536 16-bit RGB565 colors; w*h*2 buffer
 
 ssd = SSD(display_drv, format)
 
