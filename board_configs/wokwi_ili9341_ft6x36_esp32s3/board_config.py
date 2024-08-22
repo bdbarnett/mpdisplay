5d6718e4ec45431fa33c83de6f8ef47e81ec9211
Brad Barnett <bbarnett@gmail.com>
2024-08-21 21:53:08 -0500

    add broker

--- board_configs/wokwi_ili9341_ft6x36_esp32s3/board_config.py
+++ board_configs/wokwi_ili9341_ft6x36_esp32s3/board_config.py
@@ -1,10 +1,10 @@
 """ Wokwi ESP32S3 and ILI9341 Touchscreen Display """
 
-from lcd_bus import SPIBus
-from ili9341 import ILI9341
-from machine import Pin, I2C
-from ft6x36 import FT6x36
-from eventsys.devices import Devices
+from lcd_bus import SPIBus  # type: ignore
+from ili9341 import ILI9341  # type: ignore
+from machine import Pin, I2C  # type: ignore
+from ft6x36 import FT6x36  # type: ignore
+from eventsys.devices import Devices, Broker
 
 
 display_bus = SPIBus(
@@ -53,6 +53,8 @@ i2c = I2C(0, sda=Pin(7), scl=Pin(6), freq=100000)
 touch_drv = FT6x36(i2c)
 touch_read_func=touch_drv.get_positions
 touch_rotation_table=(6, 3, 0, 5)
+
+broker = Broker()
 
 if broker is not None:  # For troubleshooting only!
     touch_dev = broker.create_device(
