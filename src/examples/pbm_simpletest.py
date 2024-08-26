7c9cf3421fcac7f858bef1a2e54d3546c36cadb6
Brad Barnett <bbarnett@gmail.com>
2024-08-25 23:02:44 -0500

    pbm

--- src/examples/pbm_simpletest.py
+++ src/examples/pbm_simpletest.py
@@ -10,7 +10,7 @@ logo = PBM("examples/assets/micropython.pbm")
 logo.render(display_drv, 0, 0, 0xFFFF, 0x0000)
 
 # render direct to the display with fg and transparent bg
-logo.render(display_drv, display_drv.width//2, 0, 0xFFFF)
+logo.render(display_drv, 0, display_drv.height//2, 0xFFFF)
 
 
 # render to a frame buffer
@@ -19,7 +19,21 @@ fb = FrameBuffer(buf, logo.width, logo.height, RGB565)
 logo.render(fb, 0, 0, 0xFFFF)  # add a bg color if needed
 
 # blit the frame buffer to the display
-display_drv.blit_rect(buf, 0, display_drv.height//2, logo.width, logo.height)
+display_drv.blit_rect(buf, display_drv.width//3, 0, logo.width, logo.height)
 
 # blit the frame buffer to the display with transparent bg
-display_drv.blit_transparent(buf, display_drv.width//2, display_drv.height//2, logo.width, logo.height, 0x0)
+display_drv.blit_transparent(buf, display_drv.width//3, display_drv.height//2, logo.width, logo.height, 0x0)
+
+
+# blit to a frame buffer; re-use the same buffer and frame buffer but fill with blue first
+palette = FrameBuffer(memoryview(bytearray(2 * 2)), 2, 1, RGB565)
+palette.pixel(0, 0, 0x0FF0)
+palette.pixel(1, 0, 0xFFFF)
+fb.fill(0x000F)
+fb.blit(logo, 0, 0, palette.pixel(0, 0), palette)
+
+# blit the frame buffer to the display
+display_drv.blit_rect(buf, display_drv.width * 2 // 3, 0, logo.width, logo.height)
+
+# blit the frame buffer to the display
+display_drv.blit_transparent(buf, display_drv.width * 2 // 3, display_drv.height//2, logo.width, logo.height, 0x000F)
--- src/extras/pbm.py
+++ src/extras/pbm.py
@@ -29,81 +29,39 @@ Example:
     # Convert the image to a 16-bit color map
 """
 
-import math
 from array import array
+from framebuf import FrameBuffer, MONO_HLSB
 
 
-class PBM:
-    def __init__(self, filename=None, source=None, width=None, height=None):
+class PBM(FrameBuffer):
+    def __init__(self, filename):
         self._filename = filename
-        if source is not None:
-            self.width = width
-            self.height = height
-            self._buffer = source
-            if len(self._buffer) != math.ceil(width * height / 8):
-                raise ValueError("Invalid buffer size")
-            self._mv = memoryview(self._buffer)
-        elif filename is not None:
-            self.read()
-        else:
-            raise ValueError('Invalid arguments')
-
-    def read(self):
         with open(self._filename, "rb") as f:
-
             # Read the header
             header = f.read(3)[:2]
             if header != b"P1" and header != b"P4":
                 raise ValueError("Invalid PBM header")
-
             # Read the data
             if header == b"P1":
                 # ASCII
                 print("ASCII")
-                # Read the dimensions
-                dimensions = f.readline().strip().split()
-                self.width = int(dimensions[0])
-                self.height = int(dimensions[1])
-                self._buffer = array("B", [0] * math.ceil(self.width * self.height / 8))
-                self._mv = memoryview(self._buffer)
-                for i in range(self.height):
-                    line = f.readline().strip().split()
-                    for j in range(self.width):
-                        if line[j] == "1":
-                            self._mv[i * self.width + j // 8] |= 1 << (7 - j % 8)
+                raise NotImplementedError("ASCII PBM files are not supported yet")
             elif header == b"P4":
                 # Binary
-                print("Binary")
                 next_line = f.readline().strip()
-                if next_line[0] == 35:  # 35 is the ASCII code for #
-                    print(f"Comment: {next_line}")
+                while next_line[0] == 35:  # 35 is the ASCII code for #
                     next_line = f.readline().strip()
                 dimensions = next_line.split()
                 self.width = int(dimensions[0])
                 self.height = int(dimensions[1])
-                print(f"Dimensions: {self.width}x{self.height}")
                 self._buffer = array("B", f.read())
                 self._mv = memoryview(self._buffer)
-
-    def save(self, filename, binary=True):
-        if binary:
-            with open(filename, "wb") as f:
-                f.write(b"P4\n")
-                f.write(f"{self.width} {self.height}\n".encode())
-                f.write(self._buffer)
-        else:
-            with open(filename, "w"):
-                f.write("P1\n")
-                f.write(f"{self.width} {self.height}\n")
-                for i in range(self.height):
-                    for j in range(self.width):
-                        f.write("1" if self._mv[i * self.width + j // 8] & (1 << (7 - j % 8)) else "0")
-                    f.write("\n")
+        super().__init__(self._mv, self.width, self.height, MONO_HLSB)
 
     def render(self, canvas, x, y, fg=0, bg=None):
         col = row = 0
         for i in range(len(self._buffer)):
-            for bit in self.iterbits(self._buffer[i]):
+            for bit in self._bitgen(self._buffer[i]):
                 if bit:
                     canvas.pixel(x + col, y + row, fg)
                 elif bg is not None:
@@ -113,84 +71,21 @@ class PBM:
                     col = 0
                     row += 1
 
-    def iterbits(self, b):
+    @staticmethod
+    def _bitgen(b):
         """
         generator to iterate over the bits in a byte (character)
         """
-        in_char = self.reverse(b)
-        for i in range(8):
-            yield (in_char >> i) & 1
-
-    def reverse(self, b: int) -> int:
-        """
-        reverse bit order so the iterbits works
-        """
+        # reverse bit order
         b = (b & 0xF0) >> 4 | (b & 0x0F) << 4
         b = (b & 0xCC) >> 2 | (b & 0x33) << 2
         b = (b & 0xAA) >> 1 | (b & 0x55) << 1
-        return b
+        for i in range(8):
+            yield (b >> i) & 1
 
-    def map16(self, palette, to_buffer=None, x=0, y=0, stride=0):
-        # fg and bg are 16-bit colors
-        fg = palette[0]
-        bg = palette[1]
-        if to_buffer is None:
-            # to_buffer is an array of 16-bit colors
-            to_buffer = array("H", [0] * self.width * self.height)
-        to_mv = memoryview(to_buffer)
-        # stride is the number of pixels to skip in the destination buffer to get to the next line
-        if stride == 0:
-            try:
-                stride = to_buffer.width
-            except AttributeError:
-                stride = self.width
-
-        for i in range(self.height):
-            for j in range(self.width):
-                if self._mv[i * self.width + j // 8] & (1 << (7 - j % 8)):
-                    to_mv[(i + y) * stride + j + x] = fg
-                else:
-                    to_mv[(i + y) * stride + j + x] = bg
-        return to_buffer
-
-
-    def __getitem__(self, key):
-        if isinstance(key, int):
-            return self._mv[key]
-        elif isinstance(key, tuple):
-            if len(key) == 1:
-                return self._mv[key[0]]
-            elif len(key) == 2:
-                return self._mv[key[0] * self.width + key[1] // 8] & (1 << (7 - key[1] % 8))
-            elif len(key) == 4:
-                return self._mv[key[0]:key[1], key[2]:key[3]]
-        else:
-            raise ValueError("Invalid key type")
-        
-    def __setitem__(self, key, value):
-        if isinstance(key, int):
-            self._mv[key] = value
-        elif isinstance(key, tuple):
-            if len(key) == 1:
-                self._mv[key[0]] = value
-            elif len(key) == 2:
-                if value:
-                    self._mv[key[0] * self.width + key[1] // 8] |= 1 << (7 - key[1] % 8)
-                else:
-                    self._mv[key[0] * self.width + key[1] // 8] &= ~(1 << (7 - key[1] % 8))
-            elif len(key) == 4:
-                self._mv[key[0]:key[1], key[2]:key[3]] = value
-        else:
-            raise ValueError("Invalid key type")
-        
     def __str__(self):
         return f"PBM: {self.width}x{self.height}"
     
     def __repr__(self):
         return f"PBM: {self.width}x{self.height}"
-    
-    def __len__(self):
-        return len(self._buffer)
-    
-    def __iter__(self):
-        return iter(self._buffer)
+
