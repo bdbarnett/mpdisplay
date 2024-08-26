7d325bc703726e018f75a00919547df1f0e3f2ca
Brad Barnett <bbarnett@gmail.com>
2024-08-26 01:06:08 -0500

    default to rgb565

--- src/configs/color_setup.py
+++ src/configs/color_setup.py
@@ -12,9 +12,9 @@ import sys
 # Micropython also supports SSD.GS4_HMSB and SSD.GS8
 
 if sys.implementation.name == "micropython":
-    format = SSD.GS4_HMSB  # 4-bit (16 item) lookup table of 16-bit RGB565 colors; w*h/2 buffer
+    # format = SSD.GS4_HMSB  # 4-bit (16 item) lookup table of 16-bit RGB565 colors; w*h/2 buffer
     # format = SSD.GS8  # 256 8-bit RGB332 colors; w*h buffer
-    # format = SSD.RGB565  # all 65,536 16-bit RGB565 colors; w*h*2 buffer
+    format = SSD.RGB565  # all 65,536 16-bit RGB565 colors; w*h*2 buffer
 else:
     format = SSD.RGB565
 
