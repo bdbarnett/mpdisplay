f2fe0b4a3a05aff00aa822c75d683bca2fad929e
Brad Barnett <bbarnett@gmail.com>
2024-08-27 18:16:52 -0500

    .

--- src/examples/bmp565_blit.py
+++ src/examples/bmp565_blit.py
@@ -1,6 +1,6 @@
 from color_setup import ssd
 from bmp565 import BMP565
-from framebuf import FrameBuffer, RGB565
+from graphics.framebuf_plus import FrameBuffer, RGB565
 from time import sleep
 
 ssd.fill(0x0)
@@ -8,11 +8,16 @@ ssd.show()
 bmp = BMP565("examples/assets/warrior.bmp")
 fb = FrameBuffer(bmp.buffer, bmp.width, bmp.height, RGB565)
 
-while True:
-    ssd.blit(fb, 0, 0)
-    ssd.show()
-    sleep(1)
+a = ssd.blit(fb, 0, 0)
+print(a)
+ssd.show()
+sleep(1)
 
-    ssd.blit_rect(bmp[:], 0, 0, bmp.width, bmp.height)
-    ssd.show()
-    sleep(1)
+ssd.fill(0x0)
+ssd.show()
+sleep(.25)
+
+a = ssd.blit_rect(bmp[:], 0, 0, bmp.width, bmp.height)
+print(a)
+ssd.show()
+sleep(1)
