e79277be07a0555280a72136eaa1386840337a46
Brad Barnett <bbarnett@gmail.com>
2024-09-21 02:19:07 -0500

    .

---
+++ src/examples/first_test.py
@@ -0,0 +1,10 @@
+from board_config import display_drv, broker
+from random import getrandbits
+
+button_area = display_drv.fill_rect(10, 10, 100, 100, 0xF800)
+while True:
+    if evt := broker.poll():
+        if evt.type == broker.Events.MOUSEBUTTONDOWN:
+            if button_area.contains(evt.pos):
+                display_drv.fill_rect(*button_area, getrandbits(16))
+                print(f"Button pressed at {evt.pos}")
