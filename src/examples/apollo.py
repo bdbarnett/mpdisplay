5b4db25a0df886f80aa719963f92a4f657c206f2
Brad Barnett <bbarnett@gmail.com>
2024-09-20 23:53:16 -0500

    apollo agc dsky

--- src/examples/apollo.py
+++ src/examples/apollo.py
@@ -1,37 +1,47 @@
+import gc
+
+gc.collect()
+mem = gc.mem_free()
+print(f"Free memory at start: {mem:,}")
 
 import apollo_dsk as dsk
 from time import localtime
 import asyncio
+from sys import implementation
 
 
-async def main():
-    
-    last_time = (0, 0, 0, 0, 0, 0)
-    def write_time():
-        nonlocal last_time
-        y, mo, d, h, m, s, *_ = localtime()
+show_mem = True if implementation.name == "micropython" else False
+
+last_time = (0, 0, 0, 0, 0, 0)
+async def write_time():
+    global last_time
+    y, mo, d, h, m, s, *_ = localtime()
+    if s != last_time[5]:
+        dsk.write_string(f"{h:02}:{m:02}:{s:02}", dsk.data2_pos)
         if (y, mo, d) != last_time[:3]:
             dsk.write_string(f"{y-2000:02}.{mo:02}.{d:02}", dsk.data1_pos)
-        if (h, m, s) != last_time[3:]:
-            dsk.write_string(f"{h:02}:{m:02}:{s:02}", dsk.data2_pos)
-            last_time = (y, mo, d, h, m, s)
+        last_time = (y, mo, d, h, m, s)
+        gc.collect()
+        if show_mem:
+            dsk.write_string(f"{mem-gc.mem_free():7}", dsk.data3_pos)
 
+async def main():
     dsk.init_screen()
 
-    dsk.write_string("PR", dsk.prog_pos)
-    dsk.write_string("OS", dsk.verb_pos)
-    dsk.write_string("NO", dsk.noun_pos)
-
-    dsk.write_string("-987,654", dsk.data3_pos)
+    dsk.write_string("42", dsk.prog_pos)
+    dsk.write_string("01", dsk.verb_pos)
+    dsk.write_string("23", dsk.noun_pos)
 
     while True:
         await asyncio.sleep(0)
-        write_time()
+        await write_time()
         if (key := dsk.keypad.read()) is not None:
             dsk.set_acty(True)
             dsk.set_button(key, True)
+
             if key < len(dsk.light_status):
                 dsk.set_light(key)
+
             await asyncio.sleep(0.2)
             dsk.set_button(key, False)
             dsk.set_acty(False)
